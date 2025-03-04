4698ec75bbac089e30d3e8be28a0332de7699f12
Olamilekan Daramola <lekandar11@gmail.com>
2025-03-04 14:35:49 +0100

    codebase setup

--- library-backend/src/config/config.js
+++ library-backend/src/config/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8080,
   nodeEnv: process.env.NODE_ENV || "development",
 
   jwt: {
